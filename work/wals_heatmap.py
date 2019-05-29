a124ca503f8e975c99b5a60059b1db05aef0db5e
OneAdder <mikivo@list.ru>
2019-05-29 18:04:14 +0300

    heatmap image

--- work/wals_heatmap.py
+++ work/wals_heatmap.py
@@ -1,5 +1,14 @@
 import lingtypology
 
+#Get WALS page 1A
 wals = lingtypology.db_apis.Wals('1A')
 data = wals.get_df()
+#Write dataframe to CSV
 data.head().to_csv('tables/Wals1A.csv')
+
+#First initialize LingMap without languages
+m = lingtypology.LingMap()
+#Add heatmap from  the Wals data where the inventory is large
+m.add_heatmap(data[data._1A_desc == 'Large'].coordinates)
+#Save as PNG
+m.save_static('images/WalsHeatmap.png')
